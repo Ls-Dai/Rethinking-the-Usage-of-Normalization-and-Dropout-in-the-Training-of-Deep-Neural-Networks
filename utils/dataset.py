0505ad6a2816a458b4dba6854a04f27ea6b557b7
Alextrasza98 <yzx19981019@126.com>
2022-04-13 23:20:44 -0400

    general training func and cifar10

---
+++ utils/dataset.py
@@ -0,0 +1,39 @@
+import torch
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+def cifar10_dataset(BATCH_SIZE=128):
+    # CIFAR 10 statics
+    print("INFO: Loading CIFAR10 training dataset")
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+    
+    # training set
+    train_transform = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                          transforms.RandomCrop(32,4),
+                                          transforms.ToTensor(),
+                                          normalize])
+    train_dataset = datasets.CIFAR10(root='./data', 
+                                     train=True, 
+                                     transform=train_transform, 
+                                     download=True)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, 
+                                                   batch_size=BATCH_SIZE, 
+                                                   shuffle=True, 
+                                                   num_workers=1,
+                                                   pin_memory=True)
+    
+    # test set
+    print("INFO: Loading CIFAR10 test dataset")
+    test_transform = transforms.Compose([transforms.ToTensor(), normalize])
+    test_dataset = datasets.CIFAR10(root='./data', 
+                                    train=False, 
+                                    transform=test_transform, 
+                                    download=True)                                                            
+    test_dataloader = torch.utils.data.DataLoader(test_dataset,
+                                                  batch_size=BATCH_SIZE,
+                                                  shuffle=False,
+                                                  num_workers=1,
+                                                  pin_memory=True)
+                                            
+    return train_dataloader, test_dataloader
