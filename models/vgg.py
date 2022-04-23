d7d8eef72fcfc1827ae0bdc63335b41a8b3644ab
Alextrasza98 <yzx19981019@126.com>
2022-04-23 10:21:52 -0400

    Simplify training code, add results of densenet and vgg on cifar10

--- models/vgg.py
+++ models/vgg.py
@@ -3,11 +3,12 @@ import math
 import torch.nn as nn
 import torch.nn.init as init
 
+# Define VGG model architecture
 class VGG(nn.Module):
     '''
     VGG model 
     '''
-    def __init__(self, features):
+    def __init__(self, features, num_classes):
         super(VGG, self).__init__()
         self.features = features
         self.classifier = nn.Sequential(
@@ -17,7 +18,7 @@ class VGG(nn.Module):
             nn.Dropout(),
             nn.Linear(512, 512),
             nn.ReLU(True),
-            nn.Linear(512, 10),
+            nn.Linear(512, num_classes),
         )
          # Initialize weights
         for m in self.modules():
@@ -59,6 +60,74 @@ cfg = {
 }
 
 
-def vgg16_bn():
-    """VGG 16-layer model (configuration "D") with batch normalization"""
-    return VGG(make_layers(cfg['D'], batch_norm=True))
+def vgg16_bn(num_classes):
+    print("INFO: Creating VGG-16 Model")
+    model = VGG(make_layers(cfg['D'], batch_norm=True), num_classes)
+    model.model_name = "vgg16"
+    return model 
+
+
+# Define VGG model with IC layer
+class IC_layer(nn.Module):
+    def __init__(self, planes, p=0.05, dim ='2d'):
+        super(IC_layer, self).__init__()
+        if dim == '2d':
+            self.bn = nn.BatchNorm2d(planes)
+        elif dim == '1d':
+            self.bn = nn.BatchNorm1d(planes)
+        
+        self.dropout = nn.Dropout(p=p)
+    
+    def forward(self, x):
+        return self.dropout(self.bn(x))
+    
+class VGG_IC(nn.Module):
+    '''
+    VGG model 
+    '''
+    def __init__(self, features, num_classes):
+        super(VGG_IC, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            IC_layer(512, dim='1d'),
+            nn.Linear(512, 512),
+            nn.ReLU(True),
+            IC_layer(512, dim='1d'),
+            nn.Linear(512, 512),
+            nn.ReLU(True),
+            IC_layer(512, dim='1d'),
+            nn.Linear(512, num_classes),
+        )
+         # Initialize weights
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.bias.data.zero_()
+
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+
+def make_layers_ic(cfg):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            layers += [IC_layer(in_channels), conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+def vgg16_bn_ic(num_classes):
+    print("INFO: Creating VGG-16 Model with IC layer")
+    model = VGG_IC(make_layers_ic(cfg['D']), num_classes)
+    model.model_name = "vgg16_ic"
+    return model 
+
